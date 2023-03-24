7188574e894f4218bd569ee02d38e2ef84fe8bbd
RobertAj0857 <robertaj0857@gmail.com>
2023-03-24 10:38:15 +0100

    Revert "Prompt maker rewrite - Robert"
    This reverts commit 2c4cfbb46fd93b5b7b643d157e64492693345a32.

--- teknik eksamen/prompt maker.py
+++ teknik eksamen/prompt maker.py
@@ -1,36 +1,31 @@
-import random
+
 
 dice_number = {1,2,3,4,5,6}
 dice_operators = {"+","-","*","/"}
 
-game_mode = input("Bestem Game Mode: ")
-
-print(game_mode)
+game_mode = "all"
 
 def generate_prompt():
     if game_mode == "any":
         print ("any operator allowed")
-        # prompt = 
+        prompt = 
     elif game_mode == "plus_minus":
         print("only plus and minus")
-        prompt[1] = dice_operators[random(0,1)]
     elif game_mode == "div_mul":
         print("only division and multiplication")
-        prompt[1] = dice_operators[random(2,3)]
     elif game_mode == "random":
         print("choosing operator at random")
-        prompt[1] = dice_operators[random(0,3)]
 
 def calculate_prompt(prompt):
-    if prompt[1] == dice_operators[0]:
+    if prompt[1] == "+":
         result = prompt[0] + prompt[2]
         return result
-    elif prompt[1] == dice_operators[1]:
+    elif prompt[1] == "-":
         result = prompt[0] - prompt[2]
         return result
-    elif prompt[1] == dice_operators[2]:
+    elif prompt[1] == "*":
         result = prompt[0] * prompt[2]
         return result
-    elif prompt[1] == dice_operators[3]:
+    elif prompt[1] == "/":
         result = prompt[0] / prompt[2]
         return result
